4403fd2f663fdfe1900fcf959ee14e6f1116a726
Sebastian Hofer <sebastian.hofer@pluswerk.ag>
2023-03-21 11:42:24 +0100

    [TASK] Remove build.sh - not needed anymore

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t sebastianhofer86/deployer:6 .
-docker build -t sebastianhofer86/deployer:latest .
-docker push sebastianhofer86/deployer:6
-docker push sebastianhofer86/deployer:latest
